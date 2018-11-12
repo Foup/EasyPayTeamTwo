191599a899462a2b09934bafb85c926ef52abcc5
Shurenka <shurenkacv@gmail.com>
2018-11-12 22:19:48 +0200

    database connection

--- tests/test_get_initialized.py
+++ tests/test_get_initialized.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from src.PageObjects.login_page import Login
 from src.PageObjects.nav_menu import NavMenu
-import psycopg2
+from src.db_conn import DBConnection
 
 
 '''Verify that the specific address from the list can be chosen
@@ -18,14 +18,9 @@ class TestGetCounterInitialized:
         driver = self.driver
         login = Login(driver)
         login.login_as_inspector()
-        conn = psycopg2.connect(dbname="easypay_db", user="postgres",
-                                password="postgres", host="localhost")
-        cursor = conn.cursor()
-        cursor.execute("UPDATE counters SET old_value = 0,"
-                       " current_value = 0 WHERE id = 49;")
-        conn.commit()
-        print("Database was successfully updated")
-        conn.close()
+        with DBConnection() as db:
+            db.set_zero_values()
+            print("Database was successfully updated")
 
     def test_get_initialized(self):
         driver = self.driver
