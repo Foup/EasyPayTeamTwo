1d8cfa5990314a4a663333776b8d5bc03c9d30b0
Shurenka <shurenkacv@gmail.com>
2018-11-12 14:17:45 +0200

    minor changes

--- src/PageObjects/counters_page.py
+++ src/PageObjects/counters_page.py
@@ -10,12 +10,6 @@ class Counters(Page):
     def __init__(self, driver):
         super().__init__(driver)
 
-    def wait_for_login(self):
-        driver = self.driver
-        WebDriverWait(driver, 7).until(expected_conditions.presence_of_element_located
-                                       ((By.XPATH, HomePage.display_name)))
-        return self
-
     def open_counters_page(self):
         self.driver.find_element(By.XPATH, PathToCounters.menu_item).click()
         return self
@@ -23,11 +17,3 @@ class Counters(Page):
     def expand_counters_dropdown(self):
         self.driver.find_element(By.XPATH, PathToCounters.dropdown).click()
         return self
-
-    def wait_for_panel(self):
-        WebDriverWait(self.driver, 5).until(expected_conditions.presence_of_element_located
-                                            ((By.XPATH, PathToCounters.panel)))
-        return self
-
-    def find_addresses_list(self):
-        return self.driver.find_element(By.XPATH, PathToCounters.addresses_list)
--- tests/test_addresses_available.py
+++ tests/test_addresses_available.py
@@ -2,6 +2,11 @@ import time
 from selenium import webdriver
 from src.PageObjects.login_page import Login
 from src.PageObjects.counters_page import Counters
+from src.locators import HomePage, PathToCounters
+
+
+''' Verify that the list of clients addresses assigned
+ to the inspector is available.'''
 
 
 class TestAddressesAvailable:
@@ -15,11 +20,11 @@ class TestAddressesAvailable:
     def test_addresses_available(self):
         driver = self.driver
         counters = Counters(driver)
-        counters.wait_for_login()\
+        counters.waitForElement(HomePage.display_name, 'xpath')\
             .open_counters_page()\
             .expand_counters_dropdown()\
-            .wait_for_panel()
-        assert counters.find_addresses_list().is_displayed()
+            .waitForElement(PathToCounters.panel, 'xpath')
+        assert counters.isElementPresent(PathToCounters.addresses_list, 'xpath')
         time.sleep(2)
 
     def teardown(self):
