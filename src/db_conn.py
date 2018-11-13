e12416099eb4f61736f6b79a32ee58b13c4d5cda
Shurenka <44639453+Shurenka@users.noreply.github.com>
2018-11-13 13:07:56 +0200

    Update db_conn.py

--- src/db_conn.py
+++ src/db_conn.py
@@ -22,9 +22,7 @@ class DBConnection(object):
 
     def check_status_fix(self):
         self.cursor.execute("SELECT is_fixed FROM counters WHERE id=49;")
-        value = self.cursor.fetchone()[0]
-       # self.session.commit()
-        return value
+        return self.cursor.fetchone()[0]
 
     def set_fixed(self):
         self.cursor.execute("UPDATE counters SET is_fixed = TRUE"
